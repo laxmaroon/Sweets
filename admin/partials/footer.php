150df6bcf78880b8755819be24067f6f2e673e74
Karthik Rao <karapril15@gmail.com>
2023-07-26 02:46:15 +0530

    minor changes in footer and update-item

--- admin/partials/footer.php
+++ admin/partials/footer.php
@@ -1,6 +1,11 @@
 <div class="footer">
         <div class="wrapper text-center">
-        <p>All rights reserved. Dakshina Delicacy</p>
+        <p>2023. All rights reserved. Dakshina Delicacy</p>
+        <p>Made with <span class='heart'>&#10084</span> by 
+        <span class='creators'>
+            <a href="https://github.com/laxmaroon" target='_blank'>Lovia Edassery</a> and <a href="https://github.com/Code-Decode01" target='_blank'>Karthik Rao</a>
+        </span>
+        </p>
         </div>        
     </div>
 </body>
