864c8ee99cdacebc0c747c7899cd69d0bd303987
laxmaroon <102042850+laxmaroon@users.noreply.github.com>
2023-07-25 10:19:32 +0530

    Initial Commit

---
+++ js/custom.js
@@ -0,0 +1,71 @@
+function redirectToOrderPage() {
+    window.location.href = "order.html";
+}
+
+function showMessage() {
+    alert("Added successfully");
+}
+
+const Data = [
+    {
+        "item-name":"Dharwad Peda",
+        "item-desc": "Rich milk-based sweet with khoya, sugar, and cardamom.",
+        "item-price": "300"
+    },
+    {
+        "item-name":"Kozhukatta",
+        "item-desc": "Dumplings filled with grated coconut and jaggery.",
+        "item-price": "250"
+    },
+    {
+        "item-name":"Rava Unde",
+        "item-desc": "Sweet spheres made with semolina, ghee, sugar, and nuts.",
+        "item-price": "100"
+    },
+    {
+        "item-name":"Unniyappam",
+        "item-desc": "Banana fritters with rice flour, jaggery, and coconut.",
+        "item-price": "300"
+    },
+    {
+        "item-name":"Badam Payasam",
+        "item-desc": "Creamy almond pudding with milk, rice, and saffron.",
+        "item-price": "350"
+    },
+    {
+        "item-name":"Pal Payasam",
+        "item-desc": "Traditional rice pudding prepared with milk and sugar.",
+        "item-price": "300"
+    },
+    {
+        "item-name":"Pumpkin Payasam",
+        "item-desc": "A unique sweet treat blending pumpkin, jaggery, and coconut milk.",
+        "item-price": "300"
+    },
+    {
+        "item-name":"Mysore Pak",
+        "item-desc": "Decadent dessert featuring ghee, chickpea flour, and sugar.",
+        "item-price": "600"
+    },
+    {
+        "item-name":"Chiroti",
+        "item-desc": "Crispy, flaky pastry made with refined flour, ghee, and powdered sugar.",
+        "item-price": "300"
+    },
+    {
+        "item-name":"Kozhikodan Halwa",
+        "item-desc": "Semolina-based delight with ghee, cashews, raisins, and sugar.",
+        "item-price": "300"
+    },
+    {
+        "item-name":"Belagavi Kunda",
+        "item-desc": "Milk-based sweet delicacy with caramelized milk solids.",
+        "item-price": "300"
+    },
+    {
+        "item-name":"Kajjaaya",
+        "item-desc": "Deep-fried sweets made from jaggery, rice flour, and cardamom.",
+        "item-price": "300"
+    }
+    
+]
